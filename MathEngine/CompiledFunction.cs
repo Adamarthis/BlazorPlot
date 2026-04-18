0623ece771fb4252964f968c3cfbae180a27305e
Adamarthis <adamarthis@gmail.com>
2026-04-18 15:42:14 +0300

    add: compiledFunction

---
+++ MathEngine/CompiledFunction.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using System.Transactions;
+
+namespace MathEngine.Expressions
+{
+    public class CompiledFunction
+    {
+        public Func<double, double> Function { get; }
+
+        public CompiledFunction(INode rootNode)
+        {
+            var XParam = Expression.Parameter(typeof(double), "x");
+            var body = rootNode.ToLinqExpression(XParam);
+            var lambda = Expression.Lambda<Func<double, double>>(body, XParam);
+            Function = lambda.Compile();
+        }
+        
+        public double Evaluate(double x) => Function(x);
+    }
+}

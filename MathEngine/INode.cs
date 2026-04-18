80074e4163cf77022443dac53cf789d4e691a668
Adamarthis <adamarthis@gmail.com>
2026-04-18 15:30:41 +0300

    add: inode.cs added

---
+++ MathEngine/INode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MathEngine
+{
+    public interface INode
+    {
+        Expression ToLinqExpression(ParameterExpression xParam);
+    }
+}

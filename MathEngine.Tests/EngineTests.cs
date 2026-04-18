4f0c84e5cdfa6d60d2cfef3b82431933adc89a63
Adamarthis <adamarthis@gmail.com>
2026-04-18 18:33:55 +0300

    add: test file created

---
+++ MathEngine.Tests/EngineTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using MathEngine.Lexing;
+using MathEngine.Parsing;
+
+namespace MathEngine.Tests;
+
+[TestClass]
+public class EngineTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}

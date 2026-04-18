595df118b0b05b53d169237873dc0cc61fe6a007
Adamarthis <adamarthis@gmail.com>
2026-04-18 17:51:04 +0300

    rethink: moving to new folder named expression

--- MathEngine/Expressions/CompiledFunction.cs
+++ MathEngine/Expressions/CompiledFunction.cs
@@ -1,5 +1,4 @@
 ﻿using System.Linq.Expressions;
-using System.Transactions;
 
 namespace MathEngine.Expressions
 {
--- MathEngine/Expressions/Nodes.cs
+++ MathEngine/Expressions/Nodes.cs
@@ -1,8 +1,5 @@
 ﻿using MathEngine.Configuration;
-using System;
-using System.Collections.Generic;
 using System.Linq.Expressions;
-using System.Text;
 
 namespace MathEngine.Expressions
 {

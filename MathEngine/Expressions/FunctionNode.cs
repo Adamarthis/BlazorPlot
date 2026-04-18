7a1ca94f1e32ce42948ea61925a2b5ffa0a09ffb
Adamarthis <adamarthis@gmail.com>
2026-04-18 17:54:47 +0300

    add: function node

---
+++ MathEngine/Expressions/FunctionNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using MathEngine.Configuration;
+
+namespace MathEngine.Expressions
+{
+    public class FunctionNode(string functionName, INode argument) : INode
+    {
+        public Expression ToLinqExpression(ParameterExpression xParam)
+        {
+            var argExpr = argument.ToLinqExpression(xParam);
+
+            return FunctionRegistry.Compile(functionName, argExpr);
+        }
+    }
+}

d387dce177ffbc9ec8811fbdc81157e1875d3e50
Adamarthis <adamarthis@gmail.com>
2026-04-18 15:04:22 +0300

    add: Token class edit: renamed Tokesn to Token class

---
+++ MathEngine/Token.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathEngine
+{
+    public record Token(TokenType Type, string Value)
+    {
+        public override string ToString() => $"[{Type}: '{Value}']";
+    }
+}

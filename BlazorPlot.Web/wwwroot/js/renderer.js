b68bda93156f1a8ebd6676578f1e362736f8c2a1
Adamarthis <adamarthis@gmail.com>
2026-04-19 20:31:41 +0300

    add: ox oy net

--- BlazorPlot.Web/wwwroot/js/renderer.js
+++ BlazorPlot.Web/wwwroot/js/renderer.js
@@ -5,10 +5,55 @@
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     },
 
-    drawGraph: function (canvas, pointsData, color) {
-        if (!canvas || !pointsData || pointsData.length === 0) return;
+    drawGraph: function (canvas, centerX, centerY, uPP, width, height) {
         const ctx = canvas.getContext('2d');
         
+        // net
+        ctx.beginPath();
+        ctx.strokeStyle = '#e0e0e0';
+        ctx.lineWidth = 1;
+
+        const step = 1;
+        const startX = Math.floor((centerX - (width / 2) * uPP) / step) * step;
+        const endX = Math.ceil((centerX + (width / 2) * uPP) / step) * step;
+
+        for (let x = startX; x <= endX; x+=step) {
+            const px = (width / 2) + (x - centerX) / uPP;
+            ctx.moveTo(px, 0);
+            ctx.lineTo(px, height);
+        }
+
+        const startY = Math.floor((centerY - (height / 2) * uPP) / step) * step;
+        const endY = Math.floor((centerY + (height / 2) * uPP) / step) * step;
+
+        for (let y = startY; y <= endY; y+=step) {
+            const py = (height / 2) + (y - centerY) / uPP;
+            ctx.moveTo(0, py);
+            ctx.lineTo(width, py);
+        }
+
+        ctx.stroke();
+
+        // main OX OY
+        ctx.beginPath();
+        ctx.strokeStyle = '#000000';
+        ctx.lineWidth = 2;
+
+        const axisY = (height / 2) + centerY / uPP;
+        ctx.moveTo(0, axisY);
+        ctx.lineTo(width, axisY);
+
+        const axisX = (width / 2) - centerX / uPP;
+        ctx.moveTo(axisX, 0);
+        ctx.lineTo(axisX, height);
+
+        ctx.stroke();
+        },
+
+    drawGraph: function (canvas, pointsData, color) {
+        if (!canvas || !pointsData || pointsData.length < 4) return;
+        const ctx = canvas.getContext('2d');
+    
         ctx.beginPath();
         ctx.strokeStyle = color;
         ctx.lineWidth = 2;
@@ -33,4 +78,4 @@
         }
         ctx.stroke();
     }
-};
+}

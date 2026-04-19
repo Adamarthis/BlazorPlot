328e83105aadbf8c859cc2641133a46d6922a7a8
Adamarthis <adamarthis@gmail.com>
2026-04-20 01:04:50 +0300

    edit: now supports yParam

--- MathEngine/Expressions/CompiledFunction.cs
+++ MathEngine/Expressions/CompiledFunction.cs
@@ -4,16 +4,18 @@ namespace MathEngine.Expressions
 {
     public class CompiledFunction
     {
-        public Func<double, double> Function { get; }
+        public Func<double, double, double> Function { get; }
 
         public CompiledFunction(INode rootNode)
         {
-            var XParam = Expression.Parameter(typeof(double), "x");
-            var body = rootNode.ToLinqExpression(XParam);
-            var lambda = Expression.Lambda<Func<double, double>>(body, XParam);
+            var xParam = Expression.Parameter(typeof(double), "x");
+            var yParam = Expression.Parameter(typeof(double), "y");
+
+            var body = rootNode.ToLinqExpression(xParam, yParam);
+            var lambda = Expression.Lambda<Func<double, double, double>>(body, xParam, yParam);
             Function = lambda.Compile();
         }
         
-        public double Evaluate(double x) => Function(x);
+        public double Evaluate(double x, double y = 0) => Function(x, y);
     }
 }

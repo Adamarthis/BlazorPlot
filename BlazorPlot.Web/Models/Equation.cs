dfe25649c7c9584bbece119a761b130c368ad4c2
Adamarthis <adamarthis@gmail.com>
2026-04-29 11:25:10 +0300

    add: equation supports dict

--- BlazorPlot.Web/Models/Equation.cs
+++ BlazorPlot.Web/Models/Equation.cs
@@ -1,4 +1,5 @@
 ﻿using MathEngine.Expressions;
+using System.Runtime.InteropServices;
 
 namespace BlazorPlot.Web.Models
 {
@@ -12,7 +13,14 @@ namespace BlazorPlot.Web.Models
         public bool HasError { get; set; } = false;
         public string ErrorMessage { get; set; } = string.Empty;
         public bool IsImplicit { get; set; } = false;
-        public bool IsPoint {  get; set; } = false;
+        public bool IsPoint { get; set; } = false;
         public (double X, double Y)? PointCoordinates { get; set; }
+        public Dictionary<string, double> Parameters { get; set; } = new();
+
+        public double Evaluate(double x, double y = 0)
+        {
+            if (Function == null) return double.NaN;
+            return Function.Evaluate(x, y, Parameters);
+        }
     }
 }
